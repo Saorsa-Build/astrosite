271ccb71dd87624f3d49100dce9b40b44d6c6eda
Ethan Walker <81318227+EthanWalkerGit@users.noreply.github.com>
2026-03-03 14:28:06 -0500

    added www to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://saorsa.ca',
+  site: 'https://www.saorsa.ca',
   integrations: [react(), sitemap()],
 
   vite: {
